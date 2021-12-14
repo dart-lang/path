baedce9d2ca11ea2cdf54395a74eb038087777a4
Natalie Weizenbaum <nweiz@google.com>
2021-12-14 14:48:51 -0800

    Don't crash on p.toUri('') (#114)

--- test/url_test.dart
+++ test/url_test.dart
@@ -871,6 +871,7 @@ void main() {
         Uri.parse('http://foo.com/_%7B_%7D_%60_%5E_%20_%22_%25_'));
     expect(context.toUri(r'_%7B_%7D_%60_%5E_%20_%22_%25_'),
         Uri.parse('_%7B_%7D_%60_%5E_%20_%22_%25_'));
+    expect(context.toUri(''), Uri.parse(''));
   });
 
   group('prettyUri', () {
